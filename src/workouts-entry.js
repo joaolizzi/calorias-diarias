5892ece56ebf7139fbbb4b13b44e91590ca3c902
joaolizzi <joaopedrolizzi@outlook.com>
2026-08-17 10:33:04 -0300

    feat: expose workout module entry

---
+++ src/workouts-entry.js
@@ -0,0 +1,3 @@
+export { default as Workouts } from './pages/Workouts.jsx';
+export { default as WorkoutPlans } from './pages/WorkoutPlans.jsx';
+export { DEFAULT_EXERCISES, getWorkoutHistory, saveWorkout, calculateWorkoutStats, getPersonalBest } from './lib/workouts.js';
